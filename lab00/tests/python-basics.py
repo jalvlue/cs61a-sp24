04b6d39662d26c5eef53115af4c5807507ef7716
jalvlueshen <jalvlueshen@xiaoe-tech.com>
2024-08-09 17:01:57 +0800

    done with lab00

--- lab00/tests/python-basics.py
+++ lab00/tests/python-basics.py
@@ -1,34 +1,30 @@
 test = {
-  'name': 'Python Basics',
-  'points': 0,
-  'suites': [
-    {
-      'cases': [
+    "name": "Python Basics",
+    "points": 0,
+    "suites": [
         {
-          'code': r"""
+            "cases": [
+                {
+                    "code": r"""
           >>> x = 20
           >>> x + 2
-          3d3ab69a0677d75a0ef4a99e0d2d1451
-          # locked
+          22
           >>> x
-          e1ac00f801290865dd772310ea7c60e4
-          # locked
+          20
           >>> y = 5
           >>> y = y + 3
           >>> y * 2
-          309984ef0dc06025a91b127042939a0e
-          # locked
+          16
           >>> y + x
-          1c1c92ed084737be2f07abf6a2c86c2f
-          # locked
+          28
           """,
-          'hidden': False,
-          'locked': True,
-          'multiline': False
+                    "hidden": False,
+                    "locked": False,
+                    "multiline": False,
+                }
+            ],
+            "scored": False,
+            "type": "wwpp",
         }
-      ],
-      'scored': False,
-      'type': 'wwpp'
-    }
-  ]
+    ],
 }
