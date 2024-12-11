c00e646b48119413c9bcb280d9edb005a7acf7fe
jalvlueshen <jalvlueshen@xiaoe-tech.com>
2024-12-11 20:39:40 +0800

    hw07: init

---
+++ hw07/hw07.scm
@@ -0,0 +1,16 @@
+(define (square n) (* n n))
+
+(define (pow base exp) 'YOUR-CODE-HERE)
+
+(define (repeatedly-cube n x)
+  (if (zero? n)
+      x
+      (let (_________________)
+        (* y y y))))
+
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) 'YOUR-CODE-HERE)
+
+(define (caddr s) 'YOUR-CODE-HERE)
+
